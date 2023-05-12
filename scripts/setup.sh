33f99c62278b5b882f0df16ea35bdf61c193cde9
leaf.water <leaf.water@live.com>
2023-05-12 20:57:48 +0300

    обновление конфигуратора

---
+++ scripts/setup.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+echo "Клонирование зависимых репозиториев"
+#mkdir ./bsp -p || true
+#mkdir ./layers -p || true
+
+# Есть в репозитории
+#git -C ./bsp clone -b dunfell  https://github.com/vasilievsv/meta-radxa || true
+#git -C ./bsp clone -b dunfell  https://github.com/radxa/meta-radxa.git || true
+
+#git -C ./layers clone -b dunfell https://git.yoctoproject.org/poky.git || true
+#git -C ./layers clone -b dunfell https://git.openembedded.org/meta-openembedded || true
+#git -C ./layers clone -b dunfell https://github.com/YoeDistro/meta-python2.git || true
+#git -C ./layers clone -b dunfell https://git.yoctoproject.org/meta-security.git || true
+#git -C ./layers clone -b dunfell https://git.yoctoproject.org/meta-virtualization  || true
+#git -C ./layers clone -b dunfell https://github.com/DynamicDevices/meta-mono.git
